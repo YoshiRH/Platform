4ffcbaf9252d0d8798c657abf3ec07f3a76f5b2d
Yoshie <yoshuaaa7@gmail.com>
2021-07-30 13:08:02 +0200

    Player Death Script & Traps

---
+++ Assets/PlayerDeathSystem.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerDeathSystem : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Trap")
+        {
+            Death();
+        }
+    }
+
+    public void Death()
+    {
+        animator.SetTrigger("isDead");
+        rb.bodyType = RigidbodyType2D.Static;
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
