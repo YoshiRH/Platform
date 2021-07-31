5a39a5e5293683aa1a6d129589162a307b343443
Yoshie <yoshuaaa7@gmail.com>
2021-07-31 11:44:40 +0200

    Add moving platform & more traps

---
+++ Assets/Scripts/WaypointFollow.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointFollow : MonoBehaviour
+{
+    [SerializeField] private GameObject[] waypoints;
+    private int currentWaypoint = 0;
+
+    [SerializeField] private float speed = 2f;
+  
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(waypoints[currentWaypoint].transform.position, transform.position) < .1f)
+        {
+            currentWaypoint++;
+            if(currentWaypoint >= waypoints.Length)
+            {
+                currentWaypoint = 0;
+            } 
+           
+
+        }
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypoint].transform.position, Time.deltaTime * speed);
+    }
+}
